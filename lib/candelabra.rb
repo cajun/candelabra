99d9d199d6e7d9c945c6576e682b86bdebceaf05
Zac <zac@kleinpeter.org>
2011-01-20 17:07:51 -0600

    running the external process

--- lib/candelabra.rb
+++ lib/candelabra.rb
@@ -1,3 +1,21 @@
+require 'pty'
+
 module Candelabra
-  
+  module_function
+
+  def pid
+    /\d*/ =~ `ps -C pianobar`
+    $1
+  end
+
+  def start
+    `nohup pianobar &`
+  end
+
+  def stop
+    Process.kill('HUP', pid)
+  end
+
 end
+
+
--- spec/candelabra_spec.rb
+++ spec/candelabra_spec.rb
@@ -4,4 +4,19 @@ describe Candelabra do
   it 'should have a version number' do
     Candelabra::VERSION.must_be_instance_of String
   end
+
+  describe 'the process' do
+    before(:each) do
+      Candelabra.start
+    end
+
+    it 'should start pianobar' do
+      `ps -p #{Candelabra.pid}`.must_match /pianobar/
+    end
+
+    it 'should stop pianobar' do
+      Candelabra.stop
+      `ps -p #{Candelabra.pid}`.wont_match /pianobar/
+    end
+  end
 end
