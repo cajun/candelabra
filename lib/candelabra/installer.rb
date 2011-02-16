7e7332b50496114512ede4181db7594685e5ca18
Zac <zac@kleinpeter.org>
2011-02-16 06:09:20 -0600

    cleaned up some of the installer

--- lib/candelabra/installer.rb
+++ lib/candelabra/installer.rb
@@ -15,15 +15,15 @@ module Candelabra
   #     # => sudo apt-get install pianobar
   module Installer
     module_function
-    attr_reader :username, :password
 
-    CONSOLE_WIDTH = 50
+    CONSOLE_WIDTH = 70
 
     extend OSX
     extend Ubuntu
 
     # Executing the installing process.
     def run
+      Pianobar.stop_all # make sure all are off
       header
       what_is_installed?
       install_pianobar
@@ -55,11 +55,14 @@ module Candelabra
     def setup_config_file
       puts get_template 'setup_config_file'
       @username, @password  = ask( "Enter username:" ), ask( "Enter password:", false )
+      write_config_file 
+    end
 
+    def write_config_file( station = nil )
       FileUtils.mkdir_p piano_path
       FileUtils.mv config_path, backup_config_name if File.exists? config_path
       File.open( config_path, 'w' ) do |f| 
-        f.write config_template( username, password )
+        f.write config_template( station )
       end
     end
 
@@ -69,67 +72,68 @@ module Candelabra
       mkfifo( input_path )  unless input?
     end
 
-    def piano_path
-      "#{ENV['HOME']}/.config/pianobar"
-    end
-
-    def config_path
-      "#{piano_path}/config"
-    end
 
     def backup_config_name
-      [config_path, username, Dir.glob(config_path + '*').size.to_s].join('.')
+      [config_path, @username, Dir.glob(config_path + '*').size.to_s].join('.')
     end
 
     def config_template( station_id = nil )
-      @exe_path = "#{File.dirname(__FILE__)}/../../bin/candelabra"
-      @station  = station_id
-      get_template('config')
+      exe_path = "#{File.dirname(__FILE__)}/../../bin/candelabra"
+      station  = station_id
+      get_template('config', binding)
     end
 
-    def get_template( name, this_binding =nil)
+    def get_template( name, this_binding = nil )
       erb = ERB.new(File.read(File.dirname(__FILE__) + "/templates/#{name}.erb"))
-      if this_binding
-        erb.result this_binding
-      else
-        erb.result binding
-      end
+      erb.result this_binding
     end
 
-    def setup_auto_play_station
-      Pianobar.stop_all # make sure all are off
-      puts "Testing Configuration".center(CONSOLE_WIDTH + 20)
-      print "Starting Pianobar".ljust(CONSOLE_WIDTH - 5, '.')
+    def start_pianobar
+      if Pianobar.running?
+        print "Restarting Pianobar with Autostation".ljust(CONSOLE_WIDTH + 20, '.')
+        Pianobar.restart
+      else
+        print "Starting Pianobar".ljust(CONSOLE_WIDTH - 5, '.')
+        Pianobar.start
+      end
 
-      Pianobar.start
       5.times do 
         sleep(1)
         putc '.'
       end
 
       print Pianobar.running? ? "SUCCESS".green : "FAILED".red.blink
+    end
+
+    def setup_auto_play_station
+      puts "Testing Configuration".center(CONSOLE_WIDTH + 20)
+      start_pianobar
+
 
       if Pianobar.running?
-        `echo '0' > #{Installer.input_path}`
+        `echo '0' > #{Installer.input_path}` # forcing auto selection of the first station
         sleep( 2 )
         puts ''
         puts "Select Auto station".center( CONSOLE_WIDTH + 20, ' ' )
         stations = Remote.stations
         stations.each { |s| puts s }
         
-        result = ask 'Select Station and press ENTER:'
-        puts "You selected: #{stations[result.to_i]}"
+        begin
+          result = ask 'Select Station and press ENTER:'
+          raise unless result == result.to_i.to_s
+          puts "You selected: #{stations[result.to_i]}"
+        rescue
+          puts "You must enter the number of the station.".red
+          puts "You Entered: #{result.red}"
+          puts "Try again"
+          retry
+        end
 
         Remote.change_station result
-        id = Remote.station_id
 
-        File.open( config_path, 'w' ) { |f| f.write config_template( nil, nil, id ) }
+        write_config_file( Remote.station_id )
 
-        print "Restarting Pianobar with Autostation".ljust(CONSOLE_WIDTH + 20, '.')
-        Pianobar.restart
-        print Pianobar.running? ? "SUCCESS".green : "FAILED".red.blink
-        puts ""
-
+        start_pianobar
       end
     end
 
@@ -139,6 +143,8 @@ module Candelabra
       install 'pianobar'
       print pianobar? ? 'SUCCESS'.green : 'FAILED'.red.blink
     end
+
+    alias update_pianobar install_pianobar
 
     # Helper for asking a question
     #
@@ -202,16 +208,8 @@ module Candelabra
       test ?p, output_path
     end
 
-    def output_path
-      "#{ENV['HOME']}/.config/pianobar/output.fifo"
-    end
-
     def input?
       test ?p, input_path
-    end
-
-    def input_path
-      "#{ENV['HOME']}/.config/pianobar/input.fifo"
     end
 
     # Pianobar can talk  to a remote file. This  will determin if
@@ -222,13 +220,28 @@ module Candelabra
       test ?p, ctl_path
     end
 
+    def piano_path
+      "#{ENV['HOME']}/.config/pianobar"
+    end
+
+    def config_path
+      "#{piano_path}/config"
+    end
+
+    def output_path
+      "#{piano_path}/output.fifo"
+    end
+
+    def input_path
+      "#{piano_path}/input.fifo"
+    end
 
     # The path of the control file. This file must be a fifo file
     # inorder for it to be the correct ctl file.
     #
     # Returns the path to the control file
     def ctl_path
-      "#{ENV['HOME']}/.config/pianobar/ctl"
+      "#{piano_path}/ctl"
     end
   end
 end
